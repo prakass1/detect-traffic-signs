0b3e17eade49f0fee2a8f0e1d86bc6e18116a58f
Subash Prakash <subashprakash8@gmail.com>
2019-05-06 14:14:50 +0200

    Code to filter data according to class labels - P1

---
+++ 00_processing.py
@@ -0,0 +1,44 @@
+'''
+This class will ideally do initial bit to extract the csvs of the required classes
+and extract key features to create a data to feed into machine learning models
+
+Authors:
+
+
+Running system:
+python 3.6
+
+'''
+
+import csv
+import props
+import cv2
+import matplotlib.pyplot as plt
+import seaborn as sns
+ext = ".csv"
+file_const = "GT-"
+def read_image(base_path=".",roi=False):
+        image_list = []
+        class_labels = []
+
+        for val in props.classes:
+            # Read the file by using the prefix and base path
+            reader = open(base_path + val + "//" + "".join(file_const + val + ext))
+            csv_reader = csv.reader(reader, delimiter=';')
+            next(csv_reader)
+
+            for row in csv_reader:
+                im = cv2.imread(base_path + val + "//" + row[0])
+                image_list.append(im)
+                class_labels.append(row[7])
+                print("Image Filename - ", row[0])
+
+            #close somewhere finally
+            reader.close()
+        return image_list, class_labels
+
+#Main
+image_list, class_labels = read_image(base_path="F://Modules_Courses//Semester-4//Computer Vision//data//GTSRB//Final_Training//Images//")
+print("Length of image ", len(image_list))
+
+
---
+++ props.py
@@ -0,0 +1,10 @@
+## All properties goes here
+
+'''
+00003 - Speed Limit 60
+00011 - Priority through traffic inter
+00012 - Priority road starts
+00013 - Yield to cross side traffic
+'''
+
+classes = ["00003", "00011", "00012", "00013"]
