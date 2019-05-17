901c2f1d6f080c9d83027d04a1bb322535e6116f
0liverWatson <0liverWatson@users.noreply.github.com>
2019-05-17 09:41:17 +0200

    Update 00_processing.py

--- 00_processing.py
+++ 00_processing.py
@@ -15,6 +15,8 @@ import props
 import cv2
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
+
 ext = ".csv"
 file_const = "GT-"
 def read_image(base_path=".",roi=False):
@@ -22,23 +24,19 @@ def read_image(base_path=".",roi=False):
         class_labels = []
 
         for val in props.classes:
-            # Read the file by using the prefix and base path
-            reader = open(base_path + val + "//" + "".join(file_const + val + ext))
-            csv_reader = csv.reader(reader, delimiter=';')
-            next(csv_reader)
+            with open(base_path+val+"\\"+val+ext) as f:
+                csv_reader = csv.reader(f, delimiter=';')
+                next(f)
+                for row in csv_reader:
+                    row[0] = base_path + val + row[0]
+                    image_list.append(row)
+            
+        dataFrame = pd.DataFrame(image_list, columns=['Filename','Width','Height','Roi.X1','Roi.Y1','Roi.X2','Roi.Y2','ClassId'])
 
-            for row in csv_reader:
-                im = cv2.imread(base_path + val + "//" + row[0])
-                image_list.append(im)
-                class_labels.append(row[7])
-                print("Image Filename - ", row[0])
-
-            #close somewhere finally
-            reader.close()
-        return image_list, class_labels
+        return dataFrame
 
 #Main
-image_list, class_labels = read_image(base_path="")
+image_list = read_image(base_path="")
 print("Length of image ", len(image_list))
 
 
