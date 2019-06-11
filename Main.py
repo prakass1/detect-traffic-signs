ee6522f15f7a674e582f901e828895eb72cc45af
0liverWatson <0liverWatson@users.noreply.github.com>
2019-06-11 22:32:49 +0200

    Update Main.py
    Added comman line args

--- Main.py
+++ Main.py
@@ -25,15 +25,35 @@ OS: Windows/Linux
 
 import props as properties
 import machine_learning as ml
+import argparse
 
-#switch to args later
+
+# bool is not handled well using argparse. wrapper to convert str to bool value
+def str_to_bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected')
+
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--train', type=str_to_bool, help='training flag - indicates if training is to performed')
+parser.add_argument('--predict', type=str_to_bool, help='predict flag - indicates if prediction is to be performed')
+parser.add_argument('--model_location', type=str, help='directory to save the model')
+parser.add_argument('--train_base_dir', type=str, help='directory for the training data')
+parser.add_argument('--test_base_dir', type=str, help='directory for the test data')
+
+
 def main():
 
-    if properties.args == "cl":
-        initialize_args()
+    initialize_args()
 
     if properties.train == True:
-        for feature in ["gray", "hsv", "laplacian"]:
+        for feature in ["hog","gray", "hsv", "laplacian"]:
             ml.perform_training("rf", features=feature)
 
     if properties.predict == True:
@@ -42,9 +62,25 @@ def main():
 
 def initialize_args():
     
-    #add reading of args from command line
+    args = parser.parse_args()
 
-    return
+    if type(args.train) is bool:
+        properties.train = args.train
+    
+    if type(args.predict) is bool:
+        properties.predict = args.predict
+
+    if type(args.model_location) is str:
+        properties.model_location = args.model_location
+
+    if type(args.train_base_dir) is str:
+        properties.train_base_dir = args.train_base_dir
+
+    if type(args.test_base_dir) is str:
+        properties.test_base_dir = args.test_base_dir
+
+
+    
 
 #### Start of Main ####
 
